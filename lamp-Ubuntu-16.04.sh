6b1c634a8f43d9c976fe6d3aa92b0bcc153c6e09
adarwash <alidarwash@Alis-iMac.local>
2016-10-02 18:11:26 +0100

    Auto Yes on install packages

--- lamp-Ubuntu-16.04.sh
+++ lamp-Ubuntu-16.04.sh
@@ -1,2 +1,2 @@
 ​
-apt-get update && apt-get upgrade -y && sudo apt-get install apache2 mysql-server mysql-client php7.0-mysql php7.0-curl php7.0-json php7.0-cgi  php7.0 libapache2-mod-php7.0 phpmyadmin php-mbstring php-gettext && sudo phpenmod mcrypt && sudo phpenmod mbstring && sudo service apache2 restart
+apt-get update && apt-get upgrade -y && sudo apt-get install apache2 mysql-server mysql-client php7.0-mysql php7.0-curl php7.0-json php7.0-cgi  php7.0 libapache2-mod-php7.0 phpmyadmin php-mbstring php-gettext -y && sudo phpenmod mcrypt && sudo phpenmod mbstring && sudo service apache2 restart
